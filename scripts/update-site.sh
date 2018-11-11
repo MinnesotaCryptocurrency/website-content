931ae49fc2d8cb1ea28daaf3e95a1dd5a3559c7e
Jacob Lindahl <aviator9402@gmail.com>
2018-11-11 15:57:55 -0600

    update script actually works now + theme

--- scripts/update-site.sh
+++ scripts/update-site.sh
@@ -5,19 +5,19 @@ printf "" > ./commit-message.txt
 printf "\n\nAutomated update\n\n" >> ./commit-message.txt
 
 printf "python3 ./update-club-events-from-gopherlink.py <<<\n" >> ./commit-message.txt
-python ./scripts/update-club-events-from-gopherlink.py >> ./commit-message.txt
+python3 ./scripts/update-club-events-from-gopherlink.py >> ./commit-message.txt
 printf "\n>>>\n" >> ./commit-message.txt
 
 printf "python3 ./update-newsletters-from-email.py <<<\n"
-python ./scripts/update-newsletters-from-email.py >> ./commit-message.txt
+python3 ./scripts/update-newsletters-from-email.py >> ./commit-message.txt
 printf "\n>>>" >> ./commit-message.txt
 
 # compile site
 hugo
 
 # update git
-git add content/ docs/
-# git commit -m "update-site.sh: $(date) $(cat ./commit-message.txt)"
-# git push -u origin master
+git add content/ docs/ static/
+git commit -m "update-site.sh: $(date) $(cat ./commit-message.txt)"
+git push -u origin master
 
-# rm ./commit-message.txt
+rm ./commit-message.txt
