a33eed86293e5505774bb87c71fe52452f4e2cf6
Jacob Lindahl <aviator9402@gmail.com>
2018-11-11 16:44:53 -0600

    fix images

--- scripts/update-site.sh
+++ scripts/update-site.sh
@@ -5,11 +5,11 @@ printf "" > ./commit-message.txt
 printf "\n\nAutomated update\n\n" >> ./commit-message.txt
 
 printf "python3 ./update-club-events-from-gopherlink.py <<<\n" >> ./commit-message.txt
-python3 ./scripts/update-club-events-from-gopherlink.py >> ./commit-message.txt
+python ./scripts/update-club-events-from-gopherlink.py >> ./commit-message.txt
 printf "\n>>>\n" >> ./commit-message.txt
 
 printf "python3 ./update-newsletters-from-email.py <<<\n" >> ./commit-message.txt
-python3 ./scripts/update-newsletters-from-email.py >> ./commit-message.txt
+python ./scripts/update-newsletters-from-email.py >> ./commit-message.txt
 printf "\n>>>" >> ./commit-message.txt
 
 # compile site
